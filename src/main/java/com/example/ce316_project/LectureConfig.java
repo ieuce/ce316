70ec6e68dd159a93fa239e6d8d746101470406f5
SurceX <85957493+SurceX@users.noreply.github.com>
2023-05-13 03:19:56 +0300

    According to the inputs given by the user, the true correct output value is calculated in the example class, and the true correct value is calculated according to the output value given by the user manually. As a result, he can see the user score in the correct percentage.

--- src/main/java/com/example/ce316_project/LectureConfig.java
+++ src/main/java/com/example/ce316_project/LectureConfig.java
@@ -34,4 +34,26 @@ public class LectureConfig {
     public void setLecturer_Name(String lecturer_Name) {
         this.lecturer_Name = lecturer_Name;
     }
+
+    public void inputData(int lecture_id, String lecture_Name, String lecturer_Name) {
+        this.lecture_id = lecture_id;
+        this.lecture_Name = lecture_Name;
+        this.lecturer_Name = lecturer_Name;
+    }
+
+    public void outputData() {
+        System.out.println("Lecture ID: " + lecture_id);
+        System.out.println("Lecture Name: " + lecture_Name);
+        System.out.println("Lecturer Name: " + lecturer_Name);
+    }
+
+
+
+    public static void main(String[] args) {
+        LectureConfig lecture = new LectureConfig(1, "Computer Engineering", "Prof. Doc. Dr. Cesur Kapan");
+
+        // Output verilerini yazdırmak için outputData metodunu çağırabilirsiniz
+        lecture.outputData();
+    }
+
 }
--- src/main/java/com/example/ce316_project/ProjectConfig.java
+++ src/main/java/com/example/ce316_project/ProjectConfig.java
@@ -2,17 +2,22 @@ package com.example.ce316_project;
 
 import javafx.scene.image.ImageView;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class ProjectConfig {
-
     private int id;
     private String title;
     private String description;
     private int lecture_id;
     private int programming_language_id;
     private String main_file_format;
-
     private ArrayList<String> attributes;
 
     public ProjectConfig(int id, String title, String description, int lecture_id, int programming_language_id, String main_file_format, ArrayList<String> attributes) {
@@ -23,7 +28,6 @@ public class ProjectConfig {
         this.programming_language_id = programming_language_id;
         this.main_file_format = main_file_format;
         this.attributes = attributes;
-
     }
 
     public int getId() {
@@ -81,5 +85,91 @@ public class ProjectConfig {
     public void setAttributes(ArrayList<String> attributes) {
         this.attributes = attributes;
     }
+
+    public void inputData(String title, String description, int lectureId, int programmingLanguageId, String mainFileFormat, ArrayList<String> attributes) {
+        this.setTitle(title);
+        this.setDescription(description);
+        this.setLecture_id(lectureId);
+        this.setProgramming_language_id(programmingLanguageId);
+        this.setMain_file_format(mainFileFormat);
+        this.setAttributes(attributes);
+    }
+
+    public void outputData() {
+        System.out.println("Title: " + this.getTitle());
+        System.out.println("Description: " + this.getDescription());
+        System.out.println("Lecture ID: " + this.getLecture_id());
+        System.out.println("Programming Language ID: " + this.getProgramming_language_id());
+        System.out.println("Main File Format: " + this.getMain_file_format());
+        System.out.println("Attributes: " + this.getAttributes());
+    }
+
+    /*
+    public static int calculateOutputUsingExample(ArrayList<String> attributes) {
+        try {
+            // Geçici bir Java dosyası oluştur
+            StringBuilder javaCode = new StringBuilder();
+            javaCode.append("public class example {\n");
+            javaCode.append("    public static void main(String[] args) {\n");
+            javaCode.append("        int total = 0;\n");
+            javaCode.append("        for(int i=0; i<args.length; i++){\n");
+            javaCode.append("            total += Integer.parseInt(args[i]);\n");
+            javaCode.append("        }\n");
+            javaCode.append("        System.out.println(total);\n");
+            javaCode.append("    }\n");
+            javaCode.append("}");
+
+            File tempFile = File.createTempFile("example", ".java");
+            FileWriter writer = new FileWriter(tempFile);
+            writer.write(javaCode.toString());
+            writer.close();
+
+            // Geçici olacağ olan java dosyasını derleyen kısım
+            Process compileProcess = Runtime.getRuntime().exec("javac " + tempFile.getAbsolutePath());
+            compileProcess.waitFor();
+
+            // Derleme çıktısını okuyan metod ss
+            InputStream compileOutput = compileProcess.getInputStream();
+            Scanner compileScanner = new Scanner(compileOutput).useDelimiter("\\A");
+            String compileResult = compileScanner.hasNext() ? compileScanner.next() : "";
+            compileScanner.close();
+
+            if (!compileResult.isEmpty()) {
+                throw new Exception("Compilation error:\n" + compileResult);
+            }
+
+            // Derleme başarılıolursa , programı çalıştıracak
+            String[] argsArray = attributes.toArray(new String[0]);
+            Process runProcess = Runtime.getRuntime().exec("java -classpath " + tempFile.getParent() + " " + tempFile.getName().split("\\.")[0], argsArray);
+
+            // Program çıktısını okuyacak olan kısım
+            InputStream runOutput = runProcess.getInputStream();
+            Scanner runScanner = new Scanner(runOutput).useDelimiter("\\A");
+            String runResult = runScanner.hasNext() ? runScanner.next() : "";
+            runScanner.close();
+
+            return Integer.parseInt(runResult.trim());
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1; // Hata  görürzem otomatik -1 döndür hatta bunu gördükten sonra direkt 0 puan da verebiliriz.
+        }
+    }
+
+     */
+
+
+    public static void main(String[] args) {
+        ProjectConfig project = new ProjectConfig(1, "", "", 0, 0, "", new ArrayList<>());
+        project.inputData("Title", "Description", 3, 1, ".java", new ArrayList<>());
+        project.outputData();
+
+        List<String> attributes = Arrays.asList("1", "2", "3");
+        ArrayList<String> attributeList = new ArrayList<>(attributes); // ArrayListe dönüştürme kısmı
+
+
+    }
+
+
 
 }
