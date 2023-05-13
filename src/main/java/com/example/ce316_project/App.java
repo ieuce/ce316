4f8ef1def3e185afb8631aeb4351ba43220da050
Goodsea <kutsal_baran@hotmail.com>
2023-05-13 15:51:50 +0300

    extract zip

--- src/main/java/com/example/ce316_project/App.java
+++ src/main/java/com/example/ce316_project/App.java
@@ -7,9 +7,14 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.apache.lucene.queryparser.classic.ParseException;
 
@@ -29,6 +34,40 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
+        extractZipFile(new File("sample_files/PROJECTS.zip"), new File("src/main/resources/sample_files/projects"));
         launch();
     }
+
+    public static void extractZipFile(File zipFile, File outputDirectory) {
+        try {
+            ZipFile zip = new ZipFile(zipFile);
+            Enumeration<? extends ZipEntry> entries = zip.entries();
+            
+            byte[] buffer = new byte[1024];
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                File entryFile = new File(outputDirectory, entry.getName());
+                
+                if (entry.isDirectory()) {
+                    entryFile.mkdirs();
+                } else {
+                    entryFile.getParentFile().mkdirs();
+                    InputStream inputStream = zip.getInputStream(entry);
+                    FileOutputStream outputStream = new FileOutputStream(entryFile);
+                    int length;
+                    while ((length = inputStream.read(buffer)) > 0) {
+                        outputStream.write(buffer, 0, length);
+                    }
+                    outputStream.close();
+                    inputStream.close();
+                }
+            }
+            
+            zip.close();
+            System.out.println("Extraction completed successfully.");
+        } catch (IOException e) {
+            System.out.println("Error extracting zip file: " + e.getMessage());
+        }
+    }
+    
 }
